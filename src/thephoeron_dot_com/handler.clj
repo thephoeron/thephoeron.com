2635dbd05880ddf14060c6e269c19bf408e831fa
the Phoeron <thephoeron@me.com>
2015-03-24 19:52:25 -0400

    add analytics and splash page template

--- src/thephoeron_dot_com/handler.clj
+++ src/thephoeron_dot_com/handler.clj
@@ -11,8 +11,31 @@
             (cemerick.friend [workflows :as workflows]
                              [credentials :as creds])))
 
+(def analytics "
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-27928725-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+")
+
+(defn splash "//thephoeron.com/ splash page"
+  [req]
+  (html5
+   [:html {:lang "en"}
+    [:head
+     [:title "//thephoeron.com/"]]
+    [:body
+     [:h2 {:align "center"} "//thephoeron.com/"]]]))
+
 (defroutes app-routes
-  (GET "/" [] "ThePhoeron.com")
+  (GET "/" [req] (splash req))
   (route/not-found "Not Found"))
 
 (defn -main []
