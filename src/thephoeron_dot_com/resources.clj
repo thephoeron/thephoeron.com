8e27c3a6aa0267fe99c0d1c8a0edf9cc7fa30a28
the Phoeron <thephoeron@me.com>
2015-03-27 07:49:28 -0400

    add skeleton handler functions for all site sections

--- src/thephoeron_dot_com/resources.clj
+++ src/thephoeron_dot_com/resources.clj
@@ -14,4 +14,61 @@
   [req]
   (tmpl/default-page req {:title "Quantum Computing"}
     (html5
-     [:h2 {:align "center"} "Quantum Computing"])))
+     [:h2 {:align "center"} "Quantum Computing"]
+     [:p.lead "?"])))
+
+(defn physics "//thephoeron.com/ physics page"
+  [req]
+  (tmpl/default-page req {:title "Physics"}
+    (html5
+     [:h2 {:align "center"} "Physics"]
+     [:p.lead "?"])))
+
+(defn programming "//thephoeron.com/ programming page"
+  [req]
+  (tmpl/default-page req {:title "Programming"}
+    (html5
+     [:h2 {:align "center"} "Programming"]
+     [:p.lead "?"])))
+
+(defn linguistics "//thephoeron.com/ linguistics page"
+  [req]
+  (tmpl/default-page req {:title "Linguistics"}
+    (html5
+     [:h2 {:align "center"} "Linguistics"]
+     [:p.lead "?"])))
+
+(defn philosophy "//thephoeron.com/ philosophy page"
+  [req]
+  (tmpl/default-page req {:title "Philosophy"}
+    (html5
+     [:h2 {:align "center"} "Philosophy"]
+     [:p.lead "?"])))
+
+(defn music "//thephoeron.com/ music page"
+  [req]
+  (tmpl/default-page req {:title "Music"}
+    (html5
+     [:h2 {:align "center"} "Music"]
+     [:p.lead "?"])))
+
+(defn art "//thephoeron.com/ art page"
+  [req]
+  (tmpl/default-page req {:title "Art"}
+    (html5
+     [:h2 {:align "center"} "Art"]
+     [:p.lead "?"])))
+
+(defn sci-fi "//thephoeron.com/ sci-fi page"
+  [req]
+  (tmpl/default-page req {:title "Sci-Fi"}
+    (html5
+     [:h2 {:align "center"} "Sci-Fi"]
+     [:p.lead "?"])))
+
+(defn impressum "//thephoeron.com/ impressum page"
+  [req]
+  (tmpl/default-page req {:title "Impressum"}
+    (html5
+     [:h2 {:align "center"} "Impressum"]
+     [:p.lead "?"])))
