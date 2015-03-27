a19b19d35a5c5cb3c3457fa4cf5251c53fa46bdb
the Phoeron <thephoeron@me.com>
2015-03-27 07:24:36 -0400

    add hiccup imports to resources module

--- src/thephoeron_dot_com/resources.clj
+++ src/thephoeron_dot_com/resources.clj
@@ -1,2 +1,7 @@
 (ns thephoeron-dot-com.resources
+  (:use hiccup.core
+        hiccup.element
+        hiccup.form
+        hiccup.page
+        hiccup.util)
   (:require [thephoeron_dot_com.templates :as tmpl]))
