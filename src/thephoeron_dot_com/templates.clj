17af7d9f25027cdead82955dc5cdb763df8bf2f5
the Phoeron <thephoeron@me.com>
2015-03-27 06:26:52 -0400

    add skeleton function and macros to template module

--- src/thephoeron_dot_com/templates.clj
+++ src/thephoeron_dot_com/templates.clj
@@ -1,3 +1,20 @@
 (ns thephoeron-dot-com.templates
   (:use hiccup.core
         hiccup.page))
+
+(defn header "Page header template"
+  [])
+
+(defn footer "Page footer template"
+  [])
+
+(defn menu "Page menu template"
+  [])
+
+(defmacro page "Page template"
+  [body]
+  `(html5
+    ~(header)
+    ~(menu)
+    ~@body
+    ~(footer)))
