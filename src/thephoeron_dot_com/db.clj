f187096c0eff950e3b6a0e17434de4f66e8dea09
the Phoeron <thephoeron@users.noreply.github.com>
2015-05-15 13:17:35 -0400

    Update db.clj
    Add basic DB support and Pages table

--- src/thephoeron_dot_com/db.clj
+++ src/thephoeron_dot_com/db.clj
@@ -7,3 +7,28 @@
             [crypto.random :as crypto])
   (:import java.sql.DriverManager))
 
+(def cred (slurp (-> (java.io.File. "cred.txt") .getAbsolutePath)))
+
+(def db {:subprotocol "postgresql"
+         :subname "//localhost/thephoeron"
+         :username "thephoeron"
+         :password cred})
+       
+;; Users, Roles, Sessions
+
+;; Pages
+
+(defn create-page-table []
+  (jdbc/create-table-ddl
+   :pages
+   [:id :serial "PRIMARY KEY"]
+   [:title :text]
+   [:slug :text]
+   [:content :text]
+   [:plaintext :text]
+   [:user_id :serial "references users (id)"]
+   [:last_modified :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]
+   [:created_at :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]))
+
+;; Blog Posts
+
